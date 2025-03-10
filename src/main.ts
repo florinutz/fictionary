134b262023f5fcd4b3c6773b92733129c68195be
Florin <florinutz@users.noreply.github.com>
2025-03-10 12:31:24 +0100

    init2

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+export function add(a: number, b: number): number {
+  return a + b - 3;
+}
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  console.log("Add 2 + 3 =", add(2, 3));
+}
